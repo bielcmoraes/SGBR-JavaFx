3b7e38eb027088eeeb6faf157e8052663bbcb544
bielcmoraes <gcmorais66@gmail.com>
2022-05-31 16:27:23 -0300

    aaa

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Cliente extends Entidade{
 
 	private String nome;
 	private String cpf;
 	private String email;
 	private String telefone;
+	private ArrayList<Venda> compras;
+	
 	
 	//Construtores
 	public Cliente(String nome, String cpf, String email, String telefone) {
@@ -14,6 +18,7 @@ public class Cliente extends Entidade{
 		this.cpf = cpf;
 		this.email = email;
 		this.telefone = telefone;
+		this.compras = new ArrayList<Venda>();
 	}
 	
 	//Metodos
@@ -47,5 +52,13 @@ public class Cliente extends Entidade{
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	public ArrayList<Venda> getCompras() {
+		return compras;
+	}
+
+	public void setCompras(ArrayList<Venda> compras) {
+		this.compras = compras;
 	}
 }
--- src/testes/BancoDeDadosTest.java
+++ src/testes/BancoDeDadosTest.java
@@ -3,7 +3,6 @@ package testes;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 
