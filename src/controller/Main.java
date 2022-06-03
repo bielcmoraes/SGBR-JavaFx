6e8e1b588ef23dc67715e7877aff29a512e7b771
pwdlu <fernandocintra2871@gmail.com>
2022-06-03 17:00:03 -0300

    Tela Cadastro

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/MenuUsuario.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/Produtos.fxml"));
 			Scene scene = new Scene(root,640,480);
+			
 			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
---
+++ src/controller/ProdutosController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ProdutosController {
+
+    @FXML
+    private Button cadastrarButton;
+
+    @FXML
+    private Button editarButton;
+
+    @FXML
+    private Button excluirButton;
+
+    @FXML
+    private TableColumn<?, ?> fornecedores;
+
+    @FXML
+    private TableColumn<?, ?> idTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> medidaTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> nomeTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> precoTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> quantidadeTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> validadeTableColumn;
+
+    @FXML
+    private Button voltarButton;
+
+}
+
