ae15342d741b73cc7fa48f3c9668651b286e5285
bielcmoraes <gcmorais66@gmail.com>
2022-06-16 18:07:11 -0300

    resolvendo conflito parte 2

--- src/controller/MenuGerenteController.java
+++ src/controller/MenuGerenteController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class MenuGerenteController extends Tela {
 
@@ -66,33 +71,5 @@ public class MenuGerenteController extends Tela {
     @FXML
     private void deslogar(ActionEvent event) {
     	trocarTela(event, "/view/Login.fxml");
-    }
-<<<<<<< HEAD
-    
-    private void atualizarPainel(String url) {
-		try {
-			AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource(url));
-			AnchorPane.setTopAnchor(a, 0.0);
-			AnchorPane.setBottomAnchor(a, 0.0);
-			AnchorPane.setLeftAnchor(a, 0.0);
-			AnchorPane.setRightAnchor(a, 0.0);
-			painelAnchorPane.getChildren().clear();
-			painelAnchorPane.getChildren().add(a);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    private void trocarTela(ActionEvent event, String url) {
-    	Node node = (Node) event.getSource();
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource(url));
-			node.getScene().setRoot(root);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-=======
->>>>>>> branch 'main' of https://github.com/bielcmoraes/SGBR-JavaFx.git
-     
+    }     
 }
