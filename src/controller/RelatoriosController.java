ad6b0f1370826e6a1c473111bc592d66db8e6ef3
bielcmoraes <gcmorais66@gmail.com>
2022-06-30 21:23:15 -0300

    Gera relatórios implementado

--- src/controller/RelatoriosController.java
+++ src/controller/RelatoriosController.java
@@ -4,6 +4,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import exceptions.ErroGrave;
+import exceptions.RelatorioNaoGerado;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,6 +14,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
+import model.BancoDeDados;
+import model.GeraRelatorio;
 
 public class RelatoriosController implements Initializable{
 
@@ -40,9 +44,71 @@ public class RelatoriosController implements Initializable{
 
     @FXML
     void gerar(ActionEvent event) {
-    	if(!estoqueCombobox) {
     		
-    	}
+		GeraRelatorio geraRelatorio = new GeraRelatorio();
+		
+		if(estoqueComboBox.getSelectionModel().getSelectedItem().toString().equals("Estoque total")) {
+			try {
+				geraRelatorio.estoqueTotal(BancoDeDados.getInstance().getListaProdutos());
+			} catch (RelatorioNaoGerado | ErroGrave e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} else if(estoqueComboBox.getSelectionModel().getSelectedItem().toString().equals("Estoque por produto")) {
+			try {
+				geraRelatorio.estoquePorProduto(BancoDeDados.getInstance().getListaProdutos());
+			} catch (RelatorioNaoGerado | ErroGrave e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(estoqueComboBox.getSelectionModel().getSelectedItem().toString().equals("Estoque perto de vencer")) {
+			try {
+				geraRelatorio.estoqueProdutosPertoDeVencer(BancoDeDados.getInstance().getListaProdutos());
+			} catch (ErroGrave | RelatorioNaoGerado e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		if(fornecedorComboBox.getSelectionModel().getSelectedItem().toString().equals("Fornecedor por produto")) {
+			try {
+				geraRelatorio.fornecedorPorProduto(BancoDeDados.getInstance().getListaProdutos());
+			} catch (RelatorioNaoGerado | ErroGrave e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(fornecedorComboBox.getSelectionModel().getSelectedItem().toString().equals("Fornecedor por fornecedor")) {
+			try {
+				geraRelatorio.fornecedorPorFornecedor(BancoDeDados.getInstance().getListaFornecedores());
+			} catch (RelatorioNaoGerado | ErroGrave e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		if(produtoComboBox.getSelectionModel().getSelectedItem().toString().equals("Vendas total")) {
+			try {
+				geraRelatorio.vendasTotal(BancoDeDados.getInstance().getListaVendas());
+			} catch (ErroGrave | RelatorioNaoGerado e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(produtoComboBox.getSelectionModel().getSelectedItem().toString().equals("Vendas por periodo")) {
+			try {
+				geraRelatorio.vendasPorPeriodo(BancoDeDados.getInstance().getListaVendas());
+			} catch (ErroGrave e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(produtoComboBox.getSelectionModel().getSelectedItem().toString().equals("Vendas por tipo de prato")) {
+			try {
+				geraRelatorio.vendasPorTipoDePrato(BancoDeDados.getInstance().getListaVendas());
+			} catch (ErroGrave | RelatorioNaoGerado e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
     }
     
     private void preencherComboBox() {
@@ -52,7 +118,7 @@ public class RelatoriosController implements Initializable{
 		ArrayList<String> relatoriosProduto = new ArrayList<String>();
 		relatoriosEstoque.add("Estoque total");
 		relatoriosEstoque.add("Estoque por produto");
-		relatoriosEstoque.add("Estoque de vencidos");
+		relatoriosEstoque.add("Estoque perto de vencer");
 		
 		relatoriosFornecedor.add("Fornecedor por produto");
 		relatoriosFornecedor.add("Fornecedor por fornecedor");
