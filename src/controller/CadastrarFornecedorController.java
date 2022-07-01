31dd6d27663bd43a8633b1b15d4e7a95d97f4fc8
bielcmoraes <gcmorais66@gmail.com>
2022-07-01 11:57:32 -0300

    Imports nao utilizados removidos

--- src/controller/CadastrarFornecedorController.java
+++ src/controller/CadastrarFornecedorController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import exceptions.ErroGrave;
