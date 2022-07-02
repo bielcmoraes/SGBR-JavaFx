4de45f1d941c6a13ff5c5e3a11cdafde5cfb628b
pwdlu <fernandocintra2871@gmail.com>
2022-07-02 14:57:52 -0300

    telas de cadastro e edição de venda

--- src/controller/CadastrarVendaController.java
+++ src/controller/CadastrarVendaController.java
@@ -1,5 +1,64 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import model.Cliente;
+
 public class CadastrarVendaController {
+
+    @FXML
+    private Button adicionarButton;
+
+    @FXML
+    private Button cadastrarButton;
+
+    @FXML
+    private Button cancelarButton;
+
+    @FXML
+    private ComboBox<Cliente> clienteComboBox;
+
+    @FXML
+    private ComboBox<?> mdpComboBox;
+
+    @FXML
+    private AnchorPane painelAnchorPane;
+
+    @FXML
+    private ComboBox<?> pratoComboBox;
+
+    @FXML
+    private TableColumn<?, ?> pratoTableColumn;
+
+    @FXML
+    private TableView<?> pratosTableView;
+
+    @FXML
+    private Button removerButton;
+
+    @FXML
+    void adicionar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remover(ActionEvent event) {
+
+    }
 
 }
--- src/controller/EditarVendaController.java
+++ src/controller/EditarVendaController.java
@@ -1,5 +1,74 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class EditarVendaController {
+
+    @FXML
+    private Button adicionarButton;
+
+    @FXML
+    private Button cancelarButton;
+
+    @FXML
+    private ComboBox<?> clienteComboBox;
+
+    @FXML
+    private DatePicker dataDatePicker;
+
+    @FXML
+    private Button editarButton;
+
+    @FXML
+    private TextField horasTextField;
+
+    @FXML
+    private ComboBox<?> mdpComboBox;
+
+    @FXML
+    private TextField minutosTextField;
+
+    @FXML
+    private AnchorPane painelAnchorPane;
+
+    @FXML
+    private ComboBox<?> pratoComboBox;
+
+    @FXML
+    private TableColumn<?, ?> pratoTableColumn;
+
+    @FXML
+    private TableView<?> pratosTableView;
+
+    @FXML
+    private Button removerButton;
+
+    @FXML
+    void adicionar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remover(ActionEvent event) {
+
+    }
 
 }
